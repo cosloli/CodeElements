a193fbc22376a036a899a9321f3ff254d65a86b4
msr0325 <xh19980325@126.com>
2016-09-21 16:53:40 +0800

    提供SourceElement实现。

---
+++ Core/Data/SourceElement.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeElements.Data
+{
+    /// <summary>
+    /// 为源代码文件中的一个代码元素提供抽象数据基类。
+    /// </summary>
+    public abstract class SourceElement
+    {
+        /// <summary>
+        /// 获取源代码文件的名称。
+        /// </summary>
+        public string SourceFileName { get; private set; }
+
+        /// <summary>
+        /// 获取该代码元素的名称。
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// 获取该代码元素在源代码文件中定义的位置（行号）。
+        /// </summary>
+        public int SourceLine { get; private set; }
+
+        protected SourceElement(string sourceFileName, string name, int sourceLine)
+        {
+            SourceFileName = sourceFileName;
+            Name = name;
+            SourceLine = sourceLine;
+        }
+    }
+}
